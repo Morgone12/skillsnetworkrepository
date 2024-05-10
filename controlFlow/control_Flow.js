943c6163d0c054f8afac5c99cc31a6d77f750c51
Morgone12 <salazarjhorman818@gmail.com>
2024-05-10 10:54:01 -0400

    added some exercises

---
+++ controlFlow/control_Flow.js
@@ -0,0 +1,47 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin"){
+    accessLevel = "Full access garanted";
+}
+else if (userRole === "manager"){
+    accessLevel = "Limited access garanted";
+}
+else {
+    accessLevel = "No access garanted";
+}
+console.log("Access Level: ", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn){
+if(userRole === "admin"){
+    userMessage = "Welcome, Admin!";
+}
+else {
+    userMessage = "Welcome, User";
+}
+} 
+else {
+    userMessage = "Pleas log in to access the system."
+}
+console.log("User Message:", userMessage);
+
+let userType = "suscriber";
+let userCategory;
+
+switch(userType){
+    case "admin":
+        userCategory = "Admin";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "suscriber":
+        userCategory = "Suscriber";
+        break;
+    default: 
+    userCategory = "Unknown";        
+}
+console.log("User Category:", userCategory);
