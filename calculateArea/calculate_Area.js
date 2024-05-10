256d297dc20b555d23715c41eeb88c9ec56460b0
Morgone12 <salazarjhorman818@gmail.com>
2024-05-10 12:32:02 -0400

    #

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,8 @@
 let length;
 let width;
+let amount1;
+let amount2;
+let amount3;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
@@ -7,4 +10,14 @@ function calculateArea() {
     
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+function calculateTotalAmount(){
+    amount1 = parseFloat(document.getElementById('grocery1').value);
+    amount2 = parseFloat(document.getElementById('grocery2').value);
+    amount3 = parseFloat(document.getElementById('grocery3').value);
+
+    let result3 = amount1 + amount2 + amount3;
+
+    document.getElementById('result2').innerText = `The total amount is: ${result3}`;
 }
