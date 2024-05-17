bfb6e3e719b4c2fd57d76a882b2506548e843b38
Morgone12 <salazarjhorman818@gmail.com>
2024-05-17 18:49:03 -0400

    add practice task

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,15 +1,26 @@
 const employees = [
-    { id: 1, name: 'Jhorman Salazar', age: 18, department: 'IT', salary: 10000 },
-    { id: 2, name: 'Andres Salazar', age: 34, department: 'HR', salary: 40000 },
-    { id: 3, name: 'Jhomin Salazar', age: 20, department: 'Finance', salary: 45000 },
-    { id: 4, name: 'Jhormy Salazar', age: 26, department: 'IT', salary: 54000 },
-    { id: 5, name: 'Jhormancito Salazar', age: 23, department: 'HR', salary: 60000 },
+    { id: 1, name: 'Jhorman Salazar', age: 18, department: 'IT', salary: 10000, specialization: 'Java'},
+    { id: 2, name: 'Andres Salazar', age: 34, department: 'HR', salary: 40000, specialization: 'Python'},
+    { id: 3, name: 'Jhomin Salazar', age: 20, department: 'Finance', salary: 45000, specialization: 'JavaScript'},
+    { id: 4, name: 'Jhormy Salazar', age: 26, department: 'IT', salary: 54000, specialization: 'Go'},
+    { id: 5, name: 'Jhormancito Salazar', age: 23, department: 'HR', salary: 60000, specialization: 'JavaScript'},
 ];
 
 // Function to display all employees
 function displayEmployees(){
     const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+
+function findEmployeeBySpecialization(employeeSpecialization){
+    const foundEmployee = employees.filter(employee => employee.specialization === employeeSpecialization);
+    if(foundEmployee){
+        const displayEmployeeSpecialization = foundEmployee.map((employee, index) => `<p>${employee.id}:${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} --- ${employee.specialization}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML = displayEmployeeSpecialization;
+    }
+    else {
+        alert('There is not employees with that specialization, try another one.');
+    }
 }
 
 
