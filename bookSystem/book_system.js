63dd14de92445a752bd5a70071bbbef712d4ac1d
Morgone <salazarjhorman818@gmail.com>
2024-06-06 00:02:49 -0400

    book management system and practice task

---
+++ bookSystem/book_system.js
@@ -0,0 +1,45 @@
+let books = [];
+
+function addBooks(){
+    const bookName = document.getElementById('bookName').value;
+    const authorName = document.getElementById('authorName').value;
+    const bookDescription = document.getElementById('bookDescription').value;
+    const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+    if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+        const book = {
+            name: bookName,
+            authorName: authorName,
+            bookDescription: bookDescription,
+            pagesNumber: pagesNumber
+        };
+        books.push(book);
+        showbooks();
+        clearInputs();
+    } else {
+        alert('Por favor rellena todos los campos correctamente');
+    }   
+}
+
+function showbooks() {
+    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+        <p><strong>Book Name: </strong>${book.name}</p>
+        <p><strong>Author Name:</strong> ${book.authorName}</p>
+        <p><strong>Book Description:</strong> ${book.bookDescription}</p>
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+function clearInputs() {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
+
+function deletebook(){
+    const indice = document.getElementById('indice').value;
+    books.splice(indice, 1);
+    document.getElementById('indice').value = '';
+    showbooks();
+}
